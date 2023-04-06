473e49d14c738f08b8fd172957ff9ecb40f1b2c5
Monica Wolfe <mcwolfe2011@gmail.com>
2023-04-06 08:54:25 -0700

    Parameterized students.js and teachers.js

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -9,39 +9,24 @@ const pool = new Pool({
 
 
 //Result is an array of JavaScript objects.
-pool.query(`
-SELECT id, name, cohort_id
+const queryString = `
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
-LIMIT 5;
-`)
-  .then(res => {
-    console.log(res.rows);
-  })
-  .catch(err => console.error('query error', err.stack));
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
 
 
 //Add a join to get the cohort name instead of cohort_id.
-pool.query(`
-  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-  FROM students
-  JOIN cohorts ON cohorts.id = cohort_id
-  LIMIT 5;
-  `)
-  .then(res => {
-    res.rows.forEach(user => {
-      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
-    });
-  });
+//Values that come from somewhere else:
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+
+const values = [`%${cohortName}%`, limit];
 
 
-
-pool.query(`
-  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-  FROM students
-  JOIN cohorts ON cohorts.id = cohort_id
-  WHERE cohorts.name LIKE '%${process.argv[2]}%'
-  LIMIT ${process.argv[3] || 5};
-  `)
+pool.query(queryString, values)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
